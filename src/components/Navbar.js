59304c8fe8e1296e71178d0205c359b2798fe5c3
Elio <elionaoum.boujaoudeh@gmail.com>
2021-10-20 10:31:35 +0300

    Implement UseEffect to look better when resizing webpage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
@@ -17,6 +17,10 @@ function Navbar() {
         setButton(true);
       }
     };
+
+    useEffect(() => {
+      showButton();
+    }, []);
   
     window.addEventListener('resize', showButton);
 
