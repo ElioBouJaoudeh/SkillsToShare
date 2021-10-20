94b8662f2153574304201cb3dde98f8138838a51
Elio <elionaoum.boujaoudeh@gmail.com>
2021-10-20 10:29:06 +0300

    Add into App.css the style to be used

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ function Navbar() {
           <nav className='navbar'>
             <div className='navbar-container'>
               <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                TRVL
+                Skills To Share
                 <i class='fab fa-typo3' />
               </Link>
               <div className='menu-icon' onClick={handleClick}>
