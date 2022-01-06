d1b9950ea636cbed74a5ac0a63f296ed3672e881
Elio <elionaoum.boujaoudeh@gmail.com>
2022-01-06 17:28:35 +0200

    Editing upload.css file

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import FileUpload from './FileUpload';
-import '../App.css';
+import '../components/upload.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Upload= () => {
   return (
     <>
+    <video src='/videos/video-6.mp4' autoPlay loop muted />
     <Navbar/>
-    <div className='container mt-4'>
+    <div className='container'>
       
       {/* <h4 className='display-4 text-center mb-4'>
               <i className='fab fa-react' /> React File Upload
             </h4> */}
       
       <FileUpload/>
-      
     </div>
     <Footer/>
     </>
